5302406d70d31e447839db2d998f6af2c24ec903
raki-1203 <jfhdzzang@gmail.com>
2022-07-15 17:58:26 +0900

    update

--- src/utils.py
+++ src/utils.py
@@ -3,7 +3,10 @@ import pickle
 
 import pandas as pd
 from datasets import Features, Value, DatasetDict, Dataset
+from sklearn.model_selection import train_test_split
 from torch.optim.lr_scheduler import ReduceLROnPlateau, _LRScheduler
+from transformers import AutoConfig, BertTokenizer, AutoTokenizer, RobertaForSequenceClassification, \
+    AutoModelForSequenceClassification
 
 project_dir = os.path.dirname(os.path.dirname(__file__))
 data_dir = os.path.join(project_dir, 'data')
@@ -39,24 +42,73 @@ class AverageMeter(object):
 
 
 def make_dataset(args):
-    train_path = os.path.join(data_dir, args.train_file)
-    train_df = pd.read_csv(train_path)
-    valid_path = os.path.join(data_dir, args.validation_file)
-    valid_df = pd.read_csv(valid_path)
-    test_path = os.path.join(data_dir, args.test_file)
-    test_df = pd.read_csv(test_path)
+    if args.train_file:
+        train_path = os.path.join(data_dir, args.train_file)
+    if args.validation_file:
+        valid_path = os.path.join(data_dir, args.validation_file)
+    if args.test_file:
+        test_path = os.path.join(data_dir, args.test_file)
 
-    f = Features({'sentence1': Value(dtype='string', id=None),
-                  'sentence2': Value(dtype='string', id=None),
-                  'label': Value(dtype='string', id=None)})
+    if args.task_dataset == 'kornli':
+        train_df = pd.read_csv(train_path)
+        valid_df = pd.read_csv(valid_path)
+        test_df = pd.read_csv(test_path)
 
-    datasets = DatasetDict({'train': Dataset.from_pandas(train_df, features=f),
-                            'validation': Dataset.from_pandas(valid_df, features=f),
-                            'test': Dataset.from_pandas(test_df, features=f)})
+        f = Features({'sentence1': Value(dtype='string', id=None),
+                      'sentence2': Value(dtype='string', id=None),
+                      'label': Value(dtype='string', id=None)})
 
-    datasets.save_to_disk(os.path.join(data_dir, 'kornli_dataset'))
+        datasets = DatasetDict({'train': Dataset.from_pandas(train_df, features=f),
+                                'validation': Dataset.from_pandas(valid_df, features=f),
+                                'test': Dataset.from_pandas(test_df, features=f)})
+
+        datasets.save_to_disk(os.path.join(data_dir, 'kornli_dataset'))
+    elif args.task_dataset == 'ksc':
+        train_df = pd.read_csv(train_path)
+        train_df.columns = ['sentence1', 'label']
+
+        train_df, test_df = train_test_split(train_df, test_size=0.1, stratify=train_df['label'],
+                                             shuffle=True, random_state=args.seed)
+
+        train_df, valid_df = train_test_split(train_df, test_size=0.1, stratify=train_df['label'],
+                                              shuffle=True, random_state=args.seed)
+
+        f = Features({'sentence1': Value(dtype='string', id=None),
+                      'label': Value(dtype='string', id=None)})
+
+        datasets = DatasetDict({'train': Dataset.from_pandas(train_df, features=f),
+                                'validation': Dataset.from_pandas(valid_df, features=f),
+                                'test': Dataset.from_pandas(test_df, features=f)})
+
+        datasets.save_to_disk(os.path.join(data_dir, 'ksc_dataset'))
 
 
 class ReduceLROnPlateauPatch(ReduceLROnPlateau, _LRScheduler):
     def get_last_lr(self):
         return [group['lr'] for group in self.optimizer.param_groups]
+
+
+def get_config_tokenizer_model(args, num_labels):
+    config = AutoConfig.from_pretrained(args.model_name_or_path, num_labels=num_labels)
+    if args.vocab_path is not None and os.path.isdir(args.vocab_path):
+        tokenizer = BertTokenizer.from_pretrained(args.vocab_path,
+                                                  do_lower_case=False,
+                                                  unk_token='<unk>',
+                                                  sep_token='</s>',
+                                                  pad_token='<pad>',
+                                                  cls_token='<s>',
+                                                  mask_token='<mask>',
+                                                  model_max_length=args.max_length)
+    else:
+        tokenizer = AutoTokenizer.from_pretrained(args.model_name_or_path, use_fast=not args.use_slow_tokenizer)
+    if os.path.isdir(args.model_name_or_path):
+        model = RobertaForSequenceClassification.from_pretrained(args.model_name_or_path, num_labels=num_labels)
+    else:
+        model = AutoModelForSequenceClassification.from_pretrained(
+            args.model_name_or_path,
+            from_tf=bool('.ckpt' in args.model_name_or_path),
+            config=config,
+            ignore_mismatched_sizes=args.ignore_mismatched_sizes,
+        )
+
+    return config, tokenizer, model
