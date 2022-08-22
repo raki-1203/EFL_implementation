8f1554e795258f9b8875dc42408f90730993fbaf
raki-1203 <jfhdzzang@gmail.com>
2022-08-22 14:59:04 +0900

    nsmc_efl_TBERT_train R-Drop 변형 실험

--- shell/nsmc_efl_TBERT_train.sh
+++ shell/nsmc_efl_TBERT_train.sh
@@ -21,9 +21,17 @@ cd /home/heerak/workspace/EFL_implementation
 #--lr_scheduler_type ReduceLROnPlateau --patience 5
 
 # Description 1로 변경하고 tbert-base 기본 모델 사용하고 epoch 20 | rdrop 3
+#python src/efl_train.py --train_file ratings_train.csv --validation_file ratings_test.csv --task_dataset nsmc \
+#--model_name_or_path pretrain/checkpoint-2000000 --vocab_path tokenizer/version_1.9 \
+#--output_dir finetuning/tbert-base/nsmc/efl-desc1-rdrop_3-patience_5 --num_train_epochs 20 \
+#--checkpointing_steps 1000 --gradient_accumulation_steps 1 --pad_to_max_length --with_tracking --report_to wandb \
+#--run_name tbert-base-nsmc-efl-desc1-rdrop_3-patience_5 --entity raki-1203 --seed 42 --learning_rate 1e-5 \
+#--lr_scheduler_type ReduceLROnPlateau --patience 5 --rdrop_coef 3
+
+# Description 1로 변경하고 tbert-base 기본 모델 사용하고 epoch 20 | rdrop 5
 python src/efl_train.py --train_file ratings_train.csv --validation_file ratings_test.csv --task_dataset nsmc \
 --model_name_or_path pretrain/checkpoint-2000000 --vocab_path tokenizer/version_1.9 \
---output_dir finetuning/tbert-base/nsmc/efl-desc1-rdrop_3-patience_5 --num_train_epochs 20 \
+--output_dir finetuning/tbert-base/nsmc/efl-desc1-rdrop_5-patience_5 --num_train_epochs 20 \
 --checkpointing_steps 1000 --gradient_accumulation_steps 1 --pad_to_max_length --with_tracking --report_to wandb \
---run_name tbert-base-nsmc-efl-desc1-rdrop_3-patience_5 --entity raki-1203 --seed 42 --learning_rate 1e-5 \
---lr_scheduler_type ReduceLROnPlateau --patience 5 --rdrop_coef 3
+--run_name tbert-base-nsmc-efl-desc1-rdrop_5-patience_5 --entity raki-1203 --seed 42 --learning_rate 1e-5 \
+--lr_scheduler_type ReduceLROnPlateau --patience 5 --rdrop_coef 5
